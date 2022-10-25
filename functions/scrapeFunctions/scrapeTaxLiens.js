d8974ad0c6c8142f90e11dfbe7abf54956ed2cb3
Aston Jones <Aston.Jones@ellucian.com>
2022-10-24 21:51:48 -0500

    Got scrapeTaxLien to grab link from first listing and take link

--- functions/scrapeFunctions/scrapeTaxLiens.js
+++ functions/scrapeFunctions/scrapeTaxLiens.js
@@ -1,51 +1,30 @@
 const puppeteer = require('puppeteer');
 
 exports.scrapeTaxLiens = async () => {
-    console.log('This function will return data from tax liens');
-    var SS_COUNT = 0;
+  console.log('This function will return data from tax liens');
+  let ssCount = 0;
+  const width = 1024;
+  const height = 1600;
+  const htmlSelector = '.btn-link'
+  const initialPage = 'https://www.hctax.net/Property/listings/taxsalelisting'
 
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: false, 'defaultViewport' : { 'width' : width, 'height' : height }});
+  const page = await browser.newPage();
+  await page.goto(initialPage)
+  await page.screenshot({path: `${++ssCount}.png`})
 
-    await page.goto('https://www.hctax.net/Property/listings/taxsalelisting')
-    await page.screenshot({path: `${++SS_COUNT}.png`})
+  try {
+    await page.waitForSelector(htmlSelector);
+    const selectorElement = await page.$eval(htmlSelector, elem => elem.click());
+    console.log('selector', htmlSelector);
 
-    // first get all data from modals on page
-    const lengthOfRecordsOnPage = await page.evaluate(() => {
-        const accountIdElementsLength = document.querySelectorAll('form.statement').length;
-        return accountIdElementsLength
-    })
-
-    let data;
-    let array = await lengthOfRecordsOnPage
-    array.forEach(async (item, index) => {
-        await page.click(`input[name="acct"]`)[index]
-        await page.screenshot({path: `${++SS_COUNT}`})
-
-        const somedata = await page.evaluate(() => {
-            const something = document.querySelectorAll('tr')
-            return something
-        })
-
-        data.push(somedata)
-    })
-
-    return data
-
-    //After getting all Account Ids go to new pages and get data.
-    // const data = await arrayOfAccountIds.map(async (item) => {
-    //     await page.goto(`https://public.hcad.org/records/outsider/hc.asp?acct=${item}`)
-    //     await page.screenshot({ path: `${++SS_COUNT}.png` })
-
-    //     const address = await page.evaluate(() => {
-    //         const something = document.querySelector('tr');
-    //         console.log('something comes out', something)
-    //         return something
-    //     })
-
-    //     return address;
-    // })
-
-    // return data;
+    await page.waitForTimeout(5000)
+    let pages = await browser.pages();
+    let pageUrls = await pages.map(item => item.url())
+    browser.close();
+    return pageUrls
+  } catch (error){
+    console.log("error", error)
+  }
 
 }
